7a21f4739abfda5eb49eb2ca3c11783cada1ac51
LiuWilson00 <liuwei811007@gmail.com>
2024-05-30 21:28:47 +0800

    update app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import WebApp from '@twa-dev/sdk'
 import './App.css'
 
 function App() {
@@ -28,6 +29,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="card">
+        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+            Alert
+        </button>
+      </div>
     </>
   )
 }
